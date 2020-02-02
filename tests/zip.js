a21f6520e1d53189b4252f3db78d5a7654080185
bulletmys <bulletmys@yandex.ru>
2020-02-02 17:19:26 +0300

    Homework #0

---
+++ tests/zip.js
@@ -0,0 +1,107 @@
+'use strict';
+
+QUnit.module('Тестируем функцию zip', function () {
+    QUnit.test('Функция работает с единственным объектом', function (assert) {
+        assert.deepEqual(zip({}), {});
+        assert.deepEqual(zip({answer: 42}), {answer: 42});
+        assert.deepEqual(zip({name: 'Georg'}), {name: 'Georg'});
+        const obj = {
+            count: 0,
+            cost: '120$'
+        };
+        assert.deepEqual(zip(obj), obj);
+    });
+
+    QUnit.test('Функция работает с объектами среди которых есть объекты без свойств', function (assert) {
+        assert.deepEqual(zip({}, {}), {});
+        assert.deepEqual(zip({answer: 42}, {}), {answer: 42});
+        assert.deepEqual(zip({}, {answer: 42}), {answer: 42});
+        assert.deepEqual(zip({}, {answer: 42}), {answer: 42});
+        assert.deepEqual(zip({}, {}, {}, {name: 'Georg'}), {name: 'Georg'});
+
+        const obj = {
+            count: 0,
+            cost: '120$'
+        };
+
+        assert.deepEqual(zip({}, {}, {}, obj, {}, {}), obj);
+    });
+
+    QUnit.test('Функция работает с объектами со свойствами с разными именами', function (assert) {
+        const obj = {
+            count: 0,
+            cost: '120$'
+        };
+
+        assert.deepEqual(zip({count: 0}, {cost: '120$'}), obj);
+
+        const obj2 = {
+            a: 1,
+            b: 2,
+            c: null,
+            d: 4,
+            e: 5
+        };
+        assert.deepEqual(zip({a: 1}, {b: 2}, {c: null}, {d: 4}, {e: 5}), obj2);
+
+        const obj3 = {
+            name: 'age',
+            value: 42
+        };
+
+        const obj4 = {
+            prop: false,
+            attr: null
+        };
+
+        const obj5 = {
+            name: 'age',
+            value: 42,
+            prop: false,
+            attr: null
+        };
+
+        assert.deepEqual(zip(obj3, obj4), obj5);
+    });
+
+    QUnit.test('Функция правильно работает со свойствами, которые встречаются в нескольких объектах', function (assert) {
+        assert.deepEqual(zip({answer: 42}, {answer: false}), {answer: 42}, 'Значение должно браться из первого встретившегося поля');
+        assert.deepEqual(zip({age: 5}, {}, {age: 4}, {age: 72}), {age: 5});
+
+        const obj = {
+            name: 'age',
+            value: 42
+        };
+        assert.deepEqual(zip({name: 'age'}, {value: 42}, {name: 'cost'}, {value: -6}), obj);
+    });
+
+    QUnit.test('Копирование методов', function (assert) {
+        let obj = {
+            name: 'scotch.io',
+            exec: function ert() {
+                return true;
+            },
+        };
+
+        assert.deepEqual(zip(obj), obj);
+    });
+
+    QUnit.test('Глубокое копирование', function (assert) {
+        let obj = {
+            a: 1,
+            b: {
+                c: 2,
+            },
+        };
+
+        const zip_res = zip(obj);
+        assert.deepEqual(zip_res, {a: 1, b: {c: 2}});
+
+        let newObj = Object.assign({}, obj);
+
+        newObj.a = 20;
+        newObj.b.c = 30;
+
+        assert.deepEqual(zip_res, {a: 1, b: {c: 2}});
+    });
+});
